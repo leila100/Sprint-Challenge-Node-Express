12c8f6605969cc45fc135b5229bf95d3e416d15c
Leila <nb.leila10@gmail.com>
2019-03-09 12:18:14 -0600

    Add the PUT API to add a new action to a specific project

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const actionsDB = require("./actionModel")
+const projectsDB = require("../projects/projectModel")
 
 const router = express.Router()
 
@@ -25,6 +26,33 @@ router.get("/:id", async (req, res) => {
     res
       .status(500)
       .json({ error: "The action information could not be retrieved." })
+  }
+})
+
+router.post("/", async (req, res) => {
+  const action = req.body
+  if (!action.project_id || !action.description || !action.notes) {
+    res.status(400).json({
+      errorMessage:
+        "Please provide action's project_id, description, and notes."
+    })
+  } else {
+    try {
+      await projectsDB.get(action.project_id) // Check that project with project_id exist
+      if (action.description.length > 128) {
+        res.status(400).json({
+          errorMessage:
+            "Please provide a description with no more than 128 characters long"
+        })
+      } else {
+        const newAction = await actionsDB.insert(action)
+        res.status(201).json(newAction)
+      }
+    } catch {
+      res.status(500).json({
+        error: "There was an error while saving the action to the database"
+      })
+    }
   }
 })
 
