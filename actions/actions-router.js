9a9821d3d4d845237a6f6088e0c898fcdc5deda5
Leila <nb.leila10@gmail.com>
2019-03-09 12:06:23 -0600

    Add the GET API to get a specific action

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -16,4 +16,16 @@ router.get("/", async (req, res) => {
   }
 })
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params
+  try {
+    const action = await actionsDB.get(id) // Throws an error if no action
+    res.status(200).json(action)
+  } catch {
+    res
+      .status(500)
+      .json({ error: "The action information could not be retrieved." })
+  }
+})
+
 module.exports = router
